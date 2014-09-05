46a50f606c39ca1b71d6923312f0c8bf6f61b321
Kevin Martensson <kevinmartensson@gmail.com>
2014-09-05 19:44:39 +0200

    Use a super safe regex

--- test.js
+++ test.js
@@ -4,16 +4,87 @@ var urlRegex = require('./');
 var test = require('ava');
 
 test('match URLs', function (t) {
-    t.plan(3);
+    t.plan(27);
 
     var fixtures = [
-        'https://github.com',
-        'http://google.com'
+        'http://userid:password@example.com:8080',
+        'http://userid:password@example.com:8080/',
+        'http://userid@example.com',
+        'http://userid@example.com/',
+        'http://userid@example.com:8080',
+        'http://userid@example.com:8080/',
+        'http://userid:password@example.com',
+        'http://userid:password@example.com/',
+        'http://142.42.1.1/',
+        'http://142.42.1.1:8080/',
+        'http://➡.ws/䨹',
+        'http://⌘.ws',
+        'http://⌘.ws/',
+        'http://foo.com/blah_(wikipedia)#cite-1',
+        'http://foo.com/blah_(wikipedia)_blah#cite-1',
+        'http://foo.com/unicode_(✪)_in_parens',
+        'http://foo.com/(something)?after=parens',
+        'http://☺.damowmow.com/',
+        'http://code.google.com/events/#&product=browser',
+        'http://j.mp',
+        'ftp://foo.bar/baz',
+        'http://foo.bar/?q=Test%20URL-encoded%20stuff',
+        'http://مثال.إختبار',
+        'http://例子.测试',
+        '//userid:password@example.com:8080',
+        '//google.com'
     ];
 
     fixtures.forEach(function (el) {
-        t.assert(urlRegex().test(el), el);
+        t.assert(urlRegex().test(el));
     });
 
-    t.assert(urlRegex().exec('https://github.com')[0].trim() === 'https://github.com');
+    t.assert(urlRegex().exec('asasdasd //google.com')[0].trim() === '//google.com');
+});
+
+test('do not match URLs', function (t) {
+    t.plan(36);
+
+    var fixtures = [
+        'http://',
+        'http://.',
+        'http://..',
+        'http://../',
+        'http://?',
+        'http://??',
+        'http://??/',
+        'http://#',
+        'http://##',
+        'http://##/',
+        'http://foo.bar?q=Spaces should be encoded',
+        '//',
+        '//a',
+        '///a',
+        '///',
+        'http:///a',
+        'foo.com',
+        'rdar://1234',
+        'h://test',
+        'http:// shouldfail.com',
+        ':// should fail',
+        'http://-error-.invalid/',
+        'http://-a.b.co',
+        'http://a.b-.co',
+        'http://0.0.0.0',
+        'http://10.1.1.0',
+        'http://10.1.1.255',
+        'http://224.1.1.1',
+        'http://1.1.1.1.1',
+        'http://123.123.123',
+        'http://3628126748',
+        'http://.www.foo.bar/',
+        'http://www.foo.bar./',
+        'http://.www.foo.bar./',
+        'http://10.1.1.1',
+        'http://10.1.1.254'
+    ];
+
+    fixtures.forEach(function (el) {
+        t.assert(!urlRegex().test(el));
+    });
 });
