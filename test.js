25050cc4a4586be18229004f01fb5edcb39bc0bb
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-19 14:01:47 +0100

    Add support for URLs without protocol using a list of TLDs
    * Writes the list of TLDs to `tlds.json` and sorts them after length.
    * Removes support for `http://127.0.0.1`, use `http://localhost` instead.
    
    Fixes #6.

--- test.js
+++ test.js
@@ -1,10 +1,40 @@
 'use strict';
 
 var test = require('ava');
+var tlds = require('tlds');
 var urlRegex = require('./');
 
 test('match URLs', function (t) {
-	var fixtures = [
+	var fixtures = tlds.map(function (tld) {
+		return 'http://google.' + tld;
+	});
+
+	fixtures = fixtures.concat(tlds.map(function (tld) {
+		return '"http://google.' + tld + '"';
+	}));
+
+	fixtures = fixtures.concat([
+		'https://asd@xn----7sbbtkohtqhvkc8j.xn--p1ai',
+		'https://www.foo.com/foo/?bar=baz&inga=42&quux',
+		'http://142.42.1.1/',
+		'http://142.42.1.1:8080/',
+		'http://www.foo.com/wpstyle/?p=364',
+		'http://foo.com/blah_(wikipedia)#cite-1',
+		'http://foo.com/blah_(wikipedia)_blah#cite-1',
+		'http://foo.com/unicode_(✪)_in_parens',
+		'http://foo.com/(something)?after=parens',
+		'http://a.b.c.d.e.f.g.h.i.j.k.l.m.n.o.p.q.r.s.t.u.v.w.x.y.z.com',
+		'http://mw1.google.com/mw-earth-vectordb/kml-samples/gp/seattle/gigapxl/$[level]/r$[y]_c$[x].jpg',
+		'http://user:pass@example.com:123/one/two.three?q1=a1&q2=a2#body',
+		'http://www.example.xn--really-long-punycode-test-string-test-tests-123-tests-tests/',
+		'http://www.microsoft.xn--comindex-g03d.html.irongeek.com/',
+		'http://xn--h32b13vza.xn--3e0b707e/',
+		'http://xn—y3h.tk/',
+		'http://nic.xn--unup4y',
+		'http://☺.damowmow.com',
+		'http://☺.damowmow.com/',
+		'http://code.google.com/events/#&product=browser',
+		'http://foo.bar/?q=Test%20URL-encoded%20stuff',
 		'http://userid:password@example.com:8080',
 		'http://userid:password@example.com:8080/',
 		'http://userid@example.com',
@@ -13,75 +43,104 @@ test('match URLs', function (t) {
 		'http://userid@example.com:8080/',
 		'http://userid:password@example.com',
 		'http://userid:password@example.com/',
-		'http://142.42.1.1/',
-		'http://142.42.1.1:8080/',
-		'http://➡.ws/䨹',
-		'http://⌘.ws',
-		'http://⌘.ws/',
-		'http://foo.com/blah_(wikipedia)#cite-1',
-		'http://foo.com/blah_(wikipedia)_blah#cite-1',
-		'http://foo.com/unicode_(✪)_in_parens',
-		'http://foo.com/(something)?after=parens',
-		'http://☺.damowmow.com/',
-		'http://code.google.com/events/#&product=browser',
-		'http://j.mp',
-		'ftp://foo.bar/baz',
-		'http://foo.bar/?q=Test%20URL-encoded%20stuff',
-		'http://مثال.إختبار',
-		'http://例子.测试',
-		'//userid:password@example.com:8080',
+		'http://localhost',
+		'http://localhost/',
+		'http://➡.com',
+		'http://➡.com/䨹',
+		'http://⌘.com',
+		'http://⌘.com/',
+		'http://google.com',
+		'http://google.com/',
+		'http://1337.net',
+		'www.google.com',
+		'www.google.com/',
 		'//google.com',
-		'www.☺.com',
-		'www.goooooooooogle.com/yehiiiiiii',
-		'http://127.0.0.1',
-		'"//google.com"'
-	];
+		'//google.com/',
+		'//userid:password@example.com',
+		'//userid:password@example.com/',
+		'//☺.damowmow.com',
+		'//☺.damowmow.com/',
+		'localhost',
+		'localhost/'
+	]);
 
 	fixtures.forEach(function (el) {
-		t.assert(urlRegex().test(el), el);
+		if (!urlRegex().exec(el)) {
+			t.assert(false, el);
+			return;
+		}
+
+		t.assert(urlRegex().exec(el)[0] === el, el);
 	});
 
-	t.assert(urlRegex().exec('asasdasd //google.com')[0].trim() === '//google.com');
 	t.end();
 });
 
 test('do not match URLs', function (t) {
 	var fixtures = [
+		'//',
+		'///',
+		'///a',
+		'//a',
+		':// should fail',
+		'ftps://foo.bar/',
+		'h://test',
+		'http:// shouldfail.com',
+		'http://##',
+		'http://##/',
+		'http://#',
 		'http://',
+		'http://-a.b.co',
+		'http://-error-.invalid/',
 		'http://.',
 		'http://..',
 		'http://../',
+		'http://.www.foo.bar./',
+		'http://.www.foo.bar/',
+		'http:///a',
+		'http://0.0.0.0',
+		'http://1.1.1.1.1',
+		'http://10.1.1.0',
+		'http://10.1.1.1',
+		'http://10.1.1.255',
+		'http://123.123.123',
+		'http://127.0.0.1',
+		'http://224.1.1.1',
+		'http://3628126748',
+		'http://900.900.900.900/',
+		'http://142.42.1.1:8080:30/',
+		'http://224.1.1.1',
+		'http://362812.34',
+		'http://3628126748',
+		'http://900.900.900.900/',
 		'http://?',
 		'http://??',
 		'http://??/',
-		'http://#',
-		'http://##',
-		'http://##/',
-		'//',
-		'//a',
-		'///a',
-		'///',
-		'http:///a',
-		'foo.com',
-		'asd.foo.com',
-		'rdar://1234',
-		'h://test',
-		'http:// shouldfail.com',
-		':// should fail',
-		'http://-error-.invalid/',
-		'http://-a.b.co',
+		'http://?example.?om/',
+		'http://a.b--c.de/',
 		'http://a.b-.co',
-		'http://0.0.0.0',
-		'http://10.1.1.0',
-		'http://224.1.1.1',
-		'http://123.123.123',
-		'http://3628126748',
-		'http://.www.foo.bar/',
-		'http://.www.foo.bar./'
+		'http://foo.bar/foo(bar)baz quux',
+		'http://foo.bar?q=Spaces should be encoded',
+		'http://foo_bar.com',
+		'http://foo_bar.google.com',
+		'http://go/ogle.com',
+		'http://google.com/ /',
+		'http://google\\.com',
+		'http://www(google.com',
+		'http://www.example.xn--overly-long-punycode-test-string-test-tests-123-test-test123/',
+		'http://www=google.com',
+		'https://www.g.com/error\n/bleh/bleh',
+		'http://www.foo.bar./',
+		'rdar://1234',
 	];
 
 	fixtures.forEach(function (el) {
-		t.assert(!urlRegex().test(el), el);
+		if (!urlRegex().exec(el)) {
+			t.assert(true);
+			return;
+		}
+
+		t.assert(urlRegex().exec(el)[0] !== el, el);
 	});
 
 	t.end();
