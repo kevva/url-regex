611e40cb1a82f7f44267aadd5a9c46ae6208331e
André Veríssimo <afsverissimo@gmail.com>
2017-09-15 21:39:26 +0100

    removing parenthesis at end

--- index.js
+++ index.js
@@ -12,8 +12,16 @@ module.exports = opts => {
 	const domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
 	const tld = `(?:\\.${opts.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`})\\.?`;
 	const port = '(?::\\d{2,5})?';
-	const path = '(?:[/?#][^\\s"\\)]*)?';
-	const regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
+	const path = '(?:[/?#][^\\s\\)"]*)?';
+	const regex1 = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
+
+	const path2 = '(?:[/?#]([^\\s"]*\\([^\\s"]*))';
+	const regex2 = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path2}`;
+
+	const path3 = '(?:[/?#][^\\s"]*[^\\s\\)"]+)?';
+	const regex3 = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path3}`;
+
+	const regex = `(${regex2})|(${regex3})|(${regex1})`;
 
 	return opts.exact ? new RegExp(`(?:^${regex}$)`, 'i') : new RegExp(regex, 'ig');
 };
