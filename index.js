79cc73e8d3b884dc676f2d6d7d1c26e360dd0aa9
Král Tomáš <Tomas.Kral@ysoft.com>
2018-01-21 23:15:33 +0100

    Make mandatory protocol for local urls to distinguish simple words...

--- index.js
+++ index.js
@@ -5,16 +5,15 @@ const tlds = require('tlds');
 module.exports = opts => {
 	opts = Object.assign({strict: true, local: false}, opts);
 
-	const protocol = `(?:(?:[a-z]+:)?//)${opts.strict ? '' : '?'}`;
+	const protocol = '(?:(?:[a-z]+:)?//)';
 	const auth = '(?:\\S+(?::\\S*)?@)?';
 	const ip = ipRegex.v4().source;
 	const host = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)';
 	const domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
 	const tld = `(?:\\.${opts.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`})\\.?`;
-	const locals = opts.local ? host : 'localhost';
 	const port = '(?::\\d{2,5})?';
 	const path = '(?:[/?#][^\\s"]*)?';
-	const regex = `(?:${protocol}|www\\.)${auth}(?:${locals}|${ip}|${host}${domain}${tld})${port}${path}`;
+	const regex = `(?:(?:${protocol}${opts.strict ? '' : '?'}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${opts.local ? `|${protocol}${host}` : ''})${port}${path}`;
 
 	return opts.exact ? new RegExp(`(?:^${regex}$)`, 'i') : new RegExp(regex, 'ig');
 };
