2935ef8017c7d9fc8f240643d79cc6d433c8f058
Jerome Lachaud <jerome.lachaud@gmail.com>
2018-07-13 17:57:55 +0200

    Authorize http & https protocols only

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-'use strict';
-const ipRegex = require('ip-regex');
-const tlds = require('tlds');
+'use strict'
+const ipRegex = require('ip-regex')
+const tlds = require('tlds')
 
 module.exports = opts => {
-	opts = Object.assign({strict: true}, opts);
+	opts = Object.assign({ strict: true }, opts)
 
-	const protocol = `(?:(?:[a-z]+:)?//)${opts.strict ? '' : '?'}`;
-	const auth = '(?:\\S+(?::\\S*)?@)?';
-	const ip = ipRegex.v4().source;
-	const host = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)';
-	const domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
-	const tld = `(?:\\.${opts.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`})\\.?`;
-	const port = '(?::\\d{2,5})?';
-	const path = '(?:[/?#][^\\s"]*)?';
-	const regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
+	const protocol = `((https?:)?//)${opts.strict ? '' : '?'}`
+	const auth = '(?:\\S+(?::\\S*)?@)?'
+	const ip = ipRegex.v4().source
+	const host = '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)'
+	const domain =
+		'(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*'
+	const tld = `(?:\\.${
+		opts.strict
+			? '(?:[a-z\\u00a1-\\uffff]{2,})'
+			: `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`
+	})\\.?`
+	const port = '(?::\\d{2,5})?'
+	const path = '(?:[/?#][^\\s"]*)?'
+	const regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`
 
-	return opts.exact ? new RegExp(`(?:^${regex}$)`, 'i') : new RegExp(regex, 'ig');
-};
+	return opts.exact
+		? new RegExp(`(?:^${regex}$)`, 'i')
+		: new RegExp(regex, 'ig')
+}
