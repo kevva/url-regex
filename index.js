243a27425617c8b1f75abda9b46a930425bf716d
Omar Diab <od@eco.com>
2019-10-18 13:44:10 +0900

    move regex logic to make-url-regex

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 'use strict';
-const ipRegex = require('ip-regex');
 const tlds = require('tlds');
+const makeUrlRegex = require('./make-url-regex');
 
 module.exports = options => {
-	options = {
-		strict: true,
+	return makeUrlRegex({
+		tlds,
 		...options
-	};
-
-	const protocol = `(?:(?:[a-z]+:)?//)${options.strict ? '' : '?'}`;
-	const auth = '(?:\\S+(?::\\S*)?@)?';
-	const ip = ipRegex.v4().source;
-	const host = '(?:(?:[a-z\\u00a1-\\uffff0-9][-_]*)*[a-z\\u00a1-\\uffff0-9]+)';
-	const domain = '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*';
-	const tld = `(?:\\.${options.strict ? '(?:[a-z\\u00a1-\\uffff]{2,})' : `(?:${tlds.sort((a, b) => b.length - a.length).join('|')})`})\\.?`;
-	const port = '(?::\\d{2,5})?';
-	const path = '(?:[/?#][^\\s"]*)?';
-	const regex = `(?:${protocol}|www\\.)${auth}(?:localhost|${ip}|${host}${domain}${tld})${port}${path}`;
-
-	return options.exact ? new RegExp(`(?:^${regex}$)`, 'i') : new RegExp(regex, 'ig');
+	});
 };
